d04b3319bb2f0082d6ff38c2ea9945eb312fbeb3
sam-fsd <kabirusam037@gmail.com>
2023-06-07 10:48:35 -0700

    Removed exception handling

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    try:
-        for l in range(97, 123):
+    for l in range(97, 123):
             if ord(c) == l:
                 return True;
-    except:
-        return False
     return False
