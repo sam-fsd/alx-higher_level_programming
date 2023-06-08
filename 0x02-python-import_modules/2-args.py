2b8764db5f1f485fc2697cb42ba9385a4e4a21b5
sam-fsd <kabirusam037@gmail.com>
2023-06-08 11:39:04 -0700

    Grammar check if only 1 argument(2)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,8 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif len(argv) == 2:
         print("{} argument:".format(len(argv) - 1))
+        for arg in range(1, len(argv)):
+            print("{}: {}".format(arg, argv[arg]))
     else:
         print("{} arguments:".format(len(argv) - 1))
         for arg in range(1, len(argv)):
