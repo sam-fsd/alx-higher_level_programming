da7ebf209c16bf5334491f8b354dcfdd71352793
sam-fsd <kabirusam037@gmail.com>
2023-06-09 00:12:10 -0700

    pycodestyle

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -9,6 +9,5 @@ if __name__ == "__main__":
         print("0")
 
     for arg in range(1, len(argv)):
-            total += int(argv[arg])
-    
+        total += int(argv[arg])
     print("{}".format(total))
