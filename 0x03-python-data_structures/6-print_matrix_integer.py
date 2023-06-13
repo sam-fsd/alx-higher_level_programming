b2a4935ec82b67d517fcf75a2c717faab72676ff
sam-fsd <kabirusam037@gmail.com>
2023-06-13 13:31:27 -0700

    Refactpred code to use for loop

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    matrix_len = len(matrix)
-    i = 0
-
-    while i < matrix_len:
-        row_len = len(matrix[0])
-        j = 0
-        
-        while j < row_len:
-            print("{:d}".format(matrix[i][j]), end=' ')
-            j += 1
-        
+    for row in matrix:
+        for element in row:
+            print("{:d}".format(element), end=' ')
         print()
-        i += 1
