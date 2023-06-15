c128f9a8099c0b82296cc6eda86f98ede40d74ff
sam-fsd <kabirusam037@gmail.com>
2023-06-15 05:00:20 -0700

    Removed variable winner: not needed in the context

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    winner = ()
-    
     if a_dictionary == None:
         return None
+
     sorted_dict = sorted(a_dictionary.items(), key=lambda x: x[1])
     winner = sorted_dict[len(sorted_dict) - 1]
 
