bf2e3a0dc4e22f385725dcb5a2f41dc87023fb41
sam-fsd <kabirusam037@gmail.com>
2023-06-26 09:36:05 -0700

    specified the exception to catch

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,9 @@ def safe_print_list(my_list=[], x=0):
         while i < x:
             print("{}".format(my_list[i]), end="")
             i += 1
-    except:
+    except IndexError:
         pass
 
     print()
+
     return i
