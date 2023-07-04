ca316921086f9d2a42883c2886786fc6a5f577b9
sam-fsd <kabirusam037@gmail.com>
2023-07-04 02:54:20 -0700

    Proper indentation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,9 +2,9 @@
 """Defining a class named Rectangle"""
 
 class Rectangle:
-   """ This class defines a rectangle with width and height"""
+    """ This class defines a rectangle with width and height"""
 
-   def __init__(self, width=0, height=0):
+    def __init__(self, width=0, height=0):
         """This method intilizes the object with these private attributes
         
         Args:
@@ -14,28 +14,29 @@ class Rectangle:
         self.width = width
         self.height = height
 
-   @property
-   def width(self):
-       return self.__width
+    @property
+    def width(self):
+        return self.__width
 
-   @width.setter
-   def width(self, value):
-       if not isinstance(value, int):
-           raise TypeError('width must be an integer')
-       elif value < 0:
-           raise ValueError('width must be >= 0')
-       else:
-           self.__width = value
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
 
-   @property
-   def height(self):
-       return self.__height
+    @property
+    def height(self):
+        return self.__height
 
-   @height.setter
-   def height(self, value):
-       if not isinstance(value, int):
-           raise TypeError('height must be an integer')
-       elif value < 0:
-           raise ValueError('height must be >= 0')
-       else:
-           self.__height = value
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
+
