687316f4e00427160d8c22b7f34658166391783c
sam-fsd <kabirusam037@gmail.com>
2023-07-04 02:47:09 -0700

    Verification done during intitilization of object

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
             width (int): a positive integer
             height (int): a positive integer
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
    @property
    def width(self):
