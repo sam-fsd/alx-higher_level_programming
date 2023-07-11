30c8c26b7feb13cd279e30451889dc395019993c
sam-fsd <kabirusam037@gmail.com>
2023-07-11 14:30:35 -0700

    code refactor to adhere to pycodestyle

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-""" 
-Module defines a function
-that returns an object
-"""
+"""defining from_json_string function"""
+
+
 import json
 
 
 def from_json_string(my_str):
-    """Returns an object"""
+    """return json string"""
     return json.loads(my_str)
-
