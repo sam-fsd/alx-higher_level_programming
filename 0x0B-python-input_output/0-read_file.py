8d2ef3bd9adbb434025d087e8a14e4981c43e43d
sam-fsd <kabirusam037@gmail.com>
2023-07-11 12:18:41 -0700

    Refactored code to ommit redundancy

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """
 This module defines a read_file function
-
 """
 
 def read_file(filename=""):
-    """ This function reads a file and prints it to the stdout """
+    """Open a file and prints its contents to the stdout"""
     with open(filename, encoding='utf-8') as f:
-        content = f.read()
-        print(content, end='')
+        print(f.read(), end='')
