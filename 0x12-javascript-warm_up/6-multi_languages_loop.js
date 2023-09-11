3fabd97ff6e037212b7b3eed2881f6600fcf5d59
sam-fsd <kabirusam037@gmail.com>
2023-09-11 10:10:59 -0700

    script that prints 3 lines by using an array of string and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that prints 3 lines by using an array of string and a loop
+
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < 3; i++) {
+  console.log(arr[i]);
+}
