05e4346788c2b517d318bbe9e3817fd428b7f02e
sam-fsd <kabirusam037@gmail.com>
2023-09-11 09:42:34 -0700

    script that prints two arguments passed to it,

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//script that prints two arguments passed to it seperated by is
+
+const process = require('process');
+const argv = process.argv;
+
+console.log(`${argv[2]} is ${argv[3]}`);
