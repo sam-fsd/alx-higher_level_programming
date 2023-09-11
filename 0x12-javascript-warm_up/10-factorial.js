7eafcd2633abb9938e2fb1b7d174088a0315c545
sam-fsd <kabirusam037@gmail.com>
2023-09-11 12:43:28 -0700

    a script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a script that computes and prints a factorial
+
+const a = require('process').argv[2];
+function factorial (a) {
+  if (isNaN(a) || a === 1) {
+    return (1);
+  } else {
+    return (a * factorial(a - 1));
+  }
+}
+console.log(factorial(parseInt(a)));
