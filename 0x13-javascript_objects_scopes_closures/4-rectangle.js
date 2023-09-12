e2e67db2d4579309d96a5d9975e93086a8048cff
sam-fsd <kabirusam037@gmail.com>
2023-09-12 09:35:10 -0700

    added two more methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let x = 0; x < this.height; x++) {
+      let str = '';
+      for (let b = 0; b < this.width; b++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+module.exports = Rectangle;
