a2c7385cbb57e76bcc0b9c1f860be269056098fb
sam-fsd <kabirusam037@gmail.com>
2023-09-13 11:40:53 -0700

    refactor to make it simpler

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,24 +1,15 @@
 #!/usr/bin/node
 // a script that searches the second biggest integer in the list of arguments.
 
-const argv = require('process').argv;
+const argv = process.argv;
+let newArr = [];
 
-if (argv.length <= 3) {
+if (argv.length === 2 || argv.length === 3) {
   console.log(0);
 } else {
-  let biggestNum = parseInt(argv[2]);
-  let secondBiggestNum = parseInt(argv[2]);
-
   for (let i = 2; i < argv.length; i++) {
-    const currentNum = parseInt(argv[i]);
-
-    if (currentNum > biggestNum) {
-      secondBiggestNum = biggestNum;
-      biggestNum = currentNum;
-    } else if (currentNum > secondBiggestNum && currentNum !== biggestNum) {
-      secondBiggestNum = currentNum;
-    }
+    newArr.push(parseInt(argv[i]));
   }
-
-  console.log(secondBiggestNum);
+  newArr = newArr.sort();
+  console.log(newArr[newArr.length - 2]);
 }
