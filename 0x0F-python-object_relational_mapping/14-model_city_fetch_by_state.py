56852713c6dd47375971f229c32670ee3bd76ea7
sam-fsd <kabirusam037@gmail.com>
2023-09-21 22:05:19 -0700

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """
-prints all City objects from a database
+List all State objects from hbtn_0e_6_usa
 """
-
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sys import argv
+import sys
 from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 from model_city import City
 
-
 if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+        pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    rows = session.query(City, State).filter(City.state_id == State.id)\
-                                     .order_by(City.id).all()
-    for city, state in rows:
+
+    for city, state in session.query(City, State)\
+            .filter(City.state_id == State.id)\
+            .order_by(City.id).all():
         print("{}: ({}) {}".format(state.name, city.id, city.name))
-    session.close()
