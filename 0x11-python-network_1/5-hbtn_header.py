63170617df1d4346d39353095ad366e2f17e0ac8
sam-fsd <kabirusam037@gmail.com>
2023-09-29 09:45:42 -0700

    [refactor]

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,9 +2,9 @@
 """Getting header variable from url response"""
 
 import requests
-from sys import argv
+import sys
 
 if __name__ == '__main__':
-    r = requests.get(argv[1])
+    r = requests.get(sys.argv[1])
     r_headers = r.headers
     print(r_headers['X-Request-Id'])
