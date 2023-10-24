7d6740b1f01719e0b811822efdabad12169233fe
sam-fsd <kabirusam037@gmail.com>
2023-10-24 13:56:17 -0700

    [Fix} typo

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,5 +6,5 @@ const request = require('request');
 const url = process.argv[2];
 request(url, (error, response, body) => {
   if (error) console.log(error);
-  console.log('Code:', response.statusCode);
+  console.log('code:', response && response.statusCode);
 });
