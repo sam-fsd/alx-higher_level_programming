1491065524ae3ece55b719b1dad63bd398c7529a
sam-fsd <kabirusam037@gmail.com>
2023-10-24 12:16:14 -0700

    a script that reads and prints the content of a file.

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// a script that reads and prints the content of a file.
+
+const fs = require('fs');
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
