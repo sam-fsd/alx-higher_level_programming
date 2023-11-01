dc0d92d35b42b37eb1b6891f1025b31c454d612e
sam-fsd <kabirusam037@gmail.com>
2023-11-01 23:43:19 +0300

    a JS script that toggles a class base on the class it has

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,16 @@
+header = $('header');
+$('#toggle_header').on('click', function () {
+  ToggleClass();
+});
+
+function ToggleClass() {
+  if (header.hasClass('red')) {
+    header.removeClass('red');
+    header.addClass('green');
+  } else if (header.hasClass('green')) {
+    header.removeClass('green');
+    header.addClass('red');
+  } else {
+    header.addClass('red');
+  }
+}
